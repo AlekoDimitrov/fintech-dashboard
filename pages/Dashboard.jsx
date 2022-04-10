11f556f710a4e9d3a58c44dcd7c8c0dc3cc0de6b
Aleko Dimitrov <77157358+AlekoDimitrov@users.noreply.github.com>
2022-04-10 21:34:33 +0300

    Finish column 2 (analytics column) (unresponsive)

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -27,10 +27,15 @@ import {
   AiOutlineWallet,
   AiOutlineAppstore,
   AiOutlineCalendar,
+  AiOutlineArrowUp,
+  AiOutlineArrowDown,
+  AiOutlineBell,
+  AiOutlineSearch,
 } from "react-icons/ai";
 import { DashboardChart } from "../components/Chart.tsx";
 
 const Dashboard = () => {
+  const [display, changeDisplay] = useState("hide");
   return (
     <Flex h="100vh" flexDir="row" overflow="hidden" maxW="2000px">
       {/* Column 1 */}
@@ -42,7 +47,7 @@ const Dashboard = () => {
         backgroundColor="#020202"
         color="#ffff"
       >
-        <Flex spaceDir="row">
+        <Flex spaceDirection="row">
           <Flex flexDir="column" as="nav">
             <Heading
               mt={50}
@@ -177,20 +182,199 @@ const Dashboard = () => {
                     .00
                   </Td>
                 </Tr>
+                <Tr>
+                  <Td>
+                    <Flex>
+                      <Avatar
+                        size="md"
+                        mr={2}
+                        src="/assets/nojovetebg-logo.jpg"
+                      ></Avatar>
+                      <Flex flexDir="column" ml={6}>
+                        <Heading size="md" letterSpacing="tight">
+                          nojovete.bg
+                        </Heading>
+                        <Text color="gray" fontSize="sm">
+                          Apr 24, 2022 at 16:00
+                        </Text>
+                      </Flex>
+                    </Flex>
+                  </Td>
+                  <Td fontSize="sm">Kitchen Supplies</Td>
+                  <Td isNumeric fontWeight="bold">
+                    +$6.00
+                  </Td>
+                  <Td isNumeric>
+                    <Text fontWeight="bold" display="inline-table">
+                      -$326
+                    </Text>
+                    .99
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>
+                    <Flex>
+                      <Avatar
+                        size="md"
+                        mr={2}
+                        src="/assets/tojiro-logo.jpg"
+                      ></Avatar>
+                      <Flex flexDir="column" ml={6}>
+                        <Heading size="md" letterSpacing="tight">
+                          Tojiro
+                        </Heading>
+                        <Text color="gray" fontSize="sm">
+                          Apr 24, 2022 at 16:00
+                        </Text>
+                      </Flex>
+                    </Flex>
+                  </Td>
+                  <Td fontSize="sm">Kitchen Supplies</Td>
+                  <Td isNumeric fontWeight="bold">
+                    +$12.00
+                  </Td>
+                  <Td isNumeric>
+                    <Text fontWeight="bold" display="inline-table">
+                      -$615
+                    </Text>
+                    .00
+                  </Td>
+                </Tr>
+                {display === "show" && (
+                  <>
+                    <Tr>
+                      <Td>
+                        <Flex>
+                          <Avatar
+                            size="md"
+                            mr={2}
+                            src="/assets/vitalik-avatar.jpg"
+                          ></Avatar>
+                          <Flex flexDir="column" ml={6}>
+                            <Heading size="md" letterSpacing="tight">
+                              Ethereum
+                            </Heading>
+                            <Text color="gray" fontSize="sm">
+                              Apr 21, 2022 at 12:00
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Td>
+                      <Td fontSize="sm">Exchange</Td>
+                      <Td isNumeric fontWeight="bold">
+                        +$12563.00
+                      </Td>
+                      <Td isNumeric>
+                        <Text fontWeight="bold" display="inline-table">
+                          -$12563
+                        </Text>
+                        .00
+                      </Td>
+                    </Tr>
+                    <Tr>
+                      <Td>
+                        <Flex>
+                          <Avatar
+                            size="md"
+                            mr={2}
+                            src="/assets/gme-logo.jpg"
+                          ></Avatar>
+                          <Flex flexDir="column" ml={6}>
+                            <Heading size="md" letterSpacing="tight">
+                              Gamestop
+                            </Heading>
+                            <Text color="gray" fontSize="sm">
+                              Apr 09, 2022 at 13:12
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Td>
+                      <Td fontSize="sm">Games & Toys</Td>
+                      <Td isNumeric fontWeight="bold">
+                        +$6.00
+                      </Td>
+                      <Td isNumeric>
+                        <Text fontWeight="bold" display="inline-table">
+                          -$326
+                        </Text>
+                        .99
+                      </Td>
+                    </Tr>
+                    <Tr>
+                      <Td>
+                        <Flex>
+                          <Avatar
+                            size="md"
+                            mr={2}
+                            src="/assets/netflix-logo.jpg"
+                          ></Avatar>
+                          <Flex flexDir="column" ml={6}>
+                            <Heading size="md" letterSpacing="tight">
+                              Netflix
+                            </Heading>
+                            <Text color="gray" fontSize="sm">
+                              Apr 14, 2022 at 21:35
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Td>
+                      <Td fontSize="sm">Entertainment</Td>
+                      <Td isNumeric fontWeight="bold">
+                        +$0.05
+                      </Td>
+                      <Td isNumeric>
+                        <Text fontWeight="bold" display="inline-table">
+                          -$7
+                        </Text>
+                        .00
+                      </Td>
+                    </Tr>
+                  </>
+                )}
               </Tbody>
             </Table>
+          </Flex>
+          <Flex align="center">
+            <Divider />
+            <IconButton
+              icon={
+                display === "show" ? (
+                  <AiOutlineArrowUp />
+                ) : (
+                  <AiOutlineArrowDown />
+                )
+              }
+              onClick={() => {
+                if (display === "show") {
+                  changeDisplay("none");
+                } else {
+                  changeDisplay("show");
+                }
+              }}
+            ></IconButton>
+            <Divider />
           </Flex>
         </Flex>
       </Flex>
 
       {/* Column 3 */}
-      <Flex
-        w="35%"
-        bgColor="#F5F5F5"
-        p="3%"
-        flexDir="column"
-        overflow="auto"
-      ></Flex>
+      <Flex w="35%" bgColor="#F5F5F5" p="3%" flexDir="column" overflow="auto">
+        <Flex justifyContent="center">
+          <InputGroup backgroundColor="#ffff" borderRadius="10px" w="70%">
+            <InputLeftElement
+              pointerEvents="none"
+              children={<AiOutlineSearch color="gray" size="25px" />}
+            ></InputLeftElement>
+            <Input border="none" borderRadius="10px" placeholder="Search" />
+          </InputGroup>
+          <IconButton
+            icon={<AiOutlineBell />}
+            borderRadius="50px"
+            backgroundColor="#ffff"
+            ml="10px"
+          />
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
